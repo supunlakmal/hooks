7ce6f867e8b5419ae40a6b85066d04790b1dd48d
Supun Lakmal Abesekara <supunlakmal61@gmail.com>
2025-04-24 12:00:22 +0000

    feat: add useDeviceMotion hook to index exports

--- src/index.ts
+++ src/index.ts
@@ -74,3 +74,4 @@ export { default as useScrollSpy } from "./hooks/useScrollSpy";
 export { default as useVirtualList } from "./hooks/useVirtualList";
 export { default as useDeepCompareEffect } from "./hooks/useDeepCompareEffect";
 export { default as useIsFirstRender } from "./hooks/useIsFirstRender";
+export {default as useDeviceMotion} from "./hooks/useDeviceMotion";
