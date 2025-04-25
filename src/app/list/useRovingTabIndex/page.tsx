4d03e71a034a57dbee4b8f2a06fbb0bf8253c8fc
supun <supun@dxdy.tech>
2025-04-25 10:13:50 +0000

    update

--- src/app/list/useRovingTabIndex/page.tsx
+++ src/app/list/useRovingTabIndex/page.tsx
@@ -1,24 +1,24 @@
-// "use client"
+"use client"
 import React, { useRef, useState } from "react";
 import useRovingTabIndex from "../../../hooks/useRovingTabIndex";
 
 function RovingTabIndexExample() {
   const [items] = useState(["Item 1", "Item 2", "Item 3"]);
   const containerRef = useRef<any>(null);
-  const { getTabIndex, handleKeyDown } = useRovingTabIndex(containerRef);
+  const { activeIndex } = useRovingTabIndex(containerRef);  
 
   return (
     <div>
       <h1>useRovingTabIndex Example</h1>
-      <div ref={containerRef} onKeyDown={handleKeyDown}>
-        {items.map((item, index) => (
-          <button
+      <div ref={containerRef}  >
+         {items.map((item, index) => (
+           <button
             key={item}
             role="option"
-            tabIndex={getTabIndex(index)}
+            tabIndex={index === activeIndex ? 0 : -1}
             style={{
               display: "block",
-              marginBottom: "10px",
+              marginBottom: "10px",            
               padding: "10px",
             }}
           >
