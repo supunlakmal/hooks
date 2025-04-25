7adfbe61cce73873088c56a6e83b36a18b61a7d0
supun <supun@dxdy.tech>
2025-04-25 20:12:02 +0000

    update file

--- examples/app/list/useEventListener/page.tsx
+++ examples/app/list/useEventListener/page.tsx
@@ -1,26 +1,24 @@
 "use client";
-// import React, { useState, useEffect } from "react";
-// import useEventListener from "../../../hooks/useEventListener";
+import React, { useState, useEffect } from "react";
+import useEventListener from "../../../../src/hooks/useEventListener";
 
 function EventListenerExample() {
-  // const [count, setCount] = useState(0);
-  // const [isMounted, setIsMounted] = useState(false);
+  const [count, setCount] = useState(0);
+  const [isMounted, setIsMounted] = useState(false);
 
-  // useEffect(() => {
-  //   setIsMounted(true);
-  // }, []);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-  // if (isMounted) {
-  //   useEventListener("click", () => {
-  //     setCount((prevCount) => prevCount + 1);
-  //   });
-  // }
+  if (isMounted) {
+    useEventListener("click", () => {
+      setCount((prevCount) => prevCount + 1);
+    });
+  }
 
-
-  return (
-    <div>
-      {/* <p>Click anywhere on the screen!</p>
-      <p>Counter: {count}</p> */}
+  return (<div>
+    <p>Click anywhere on the screen!</p>
+    <p>Counter: {count}</p>
     </div>
   );
 }
